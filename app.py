b7577dd27bd942a4cc65750af5b0fdecc456a267
Rafael <rcarmona50@gmail.com>
2023-04-20 16:34:48 -0700

    Setup and connect OpenAI

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
     template_folder='templates'            
 )
 
+# @app.route("/palette", methods=["POST"])
+# def get_palette():
+#     return render_template("index.html")
+
 @app.route("/")
 def index():
-    return "Hello there from flask!"
+    response = openai.Completion.create(
+        model="text-davinci-003", 
+        max_tokens = 130,
+        prompt = 'What is the capital of Puerto Rico?',
+    )
+    return response["choices"][0]["text"]
+    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
