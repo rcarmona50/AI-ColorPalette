cd92799c4dce5ddc5a92eb9b44a63d09bb6b5ca0
Rafael <rcarmona50@gmail.com>
2023-04-20 16:58:55 -0700

    Endpoint, request, form done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import openai
 from dotenv import dotenv_values
+import json
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
@@ -9,19 +10,42 @@ app = Flask(__name__,
     template_folder='templates'            
 )
 
-# @app.route("/palette", methods=["POST"])
-# def get_palette():
-#     return render_template("index.html")
+def get_colors(msg):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes
+    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
+    The palettes should be between 2 and 6 colors.
 
-@app.route("/")
-def index():
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
+
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    A:
+"""
     response = openai.Completion.create(
         model="text-davinci-003", 
         max_tokens = 130,
-        prompt = 'What is the capital of Puerto Rico?',
-    )
-    return response["choices"][0]["text"]
-    # return render_template("index.html")
+        prompt = prompt,
+        )
+
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+@app.route("/palette", methods=["POST"])
+def get_palette():
+    query = request.form.get("query")
+    colors = get_colors(query)
+    return {"colors":colors}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
