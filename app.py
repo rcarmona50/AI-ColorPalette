90780db6ae84a7199910b61bba70e203449b1805
Rafael <rcarmona50@gmail.com>
2023-04-20 16:19:34 -0700

    Setup Flask

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__,
+    template_folder='templates'            
+)
+
+@app.route("/")
+def index():
+    return "Hello there from flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
