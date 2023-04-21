594c5cf6058d04da3d8c9772eda19cde90698fb6
Rafael <rcarmona50@gmail.com>
2023-04-20 18:02:18 -0700

    Refractor. Styles Added. Finished

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request
 import openai
+from flask import Flask, render_template, request
 from dotenv import dotenv_values
 import json
 
-config = dotenv_values(".env")
+config = dotenv_values('.env');
 openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
-    template_folder='templates'            
+    template_folder='templates',
+    static_url_path='', 
+    static_folder='static'
 )
 
 def get_colors(msg):
     prompt = f"""
     You are a color palette generating assistant that responds to text prompts for color palettes
     Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
-    The palettes should be between 2 and 6 colors.
+    The palettes should be between 2 and 8 colors.
 
     Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
     A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
@@ -25,24 +27,25 @@ def get_colors(msg):
 
     Desired Format: a JSON array of hexadecimal color codes
 
-    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg} 
     A:
-"""
+    """
+
     response = openai.Completion.create(
-        model="text-davinci-003", 
-        max_tokens = 130,
-        prompt = prompt,
-        )
+        prompt=prompt,
+        model="text-davinci-003",
+        max_tokens=200,
+    )
 
     colors = json.loads(response["choices"][0]["text"])
     return colors
 
 @app.route("/palette", methods=["POST"])
-def get_palette():
+def prompt_to_palette():
     query = request.form.get("query")
     colors = get_colors(query)
-    return {"colors":colors}
-
+    return {"colors": colors}
+    
 @app.route("/")
 def index():
     return render_template("index.html")
---
+++ static/app.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("#form");
+        form.addEventListener("submit", function(e){
+            e.preventDefault();
+            query = form.elements.query.value;
+            fetch("/palette", {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({
+                    query: query
+                })
+            })
+            .then((response) => response.json())
+            .then(data =>{
+                const colors = data.colors;
+                const container = document.querySelector(".container");
+                container.innerHTML = "";
+                for(const color of colors){
+                    const div = document.createElement("div");
+                    div.classList.add("color")
+                    div.style.backgroundColor = color;
+                    div.style.width = `calc(100% / ${colors.length})`
+
+                    div.addEventListener("click", function(){
+                        navigator.clipboard.writeText(color);
+                    })
+
+                    const span = document.createElement("span");
+                    span.innerText = color;
+                    div.appendChild(span);
+
+                    container.appendChild(div);
+                }
+            })
+        })
